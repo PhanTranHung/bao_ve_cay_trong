b62053b39c587d279e46be4dae8366ebb6e5c2fb
trhieu310 <58017675+trhieu310@users.noreply.github.com>
2021-05-31 18:31:56 +0700

    draw model

--- back_end/flask_app/app.py
+++ back_end/flask_app/app.py
@@ -11,25 +11,33 @@ import imutils
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 from werkzeug.utils import redirect, secure_filename
+from firebase_admin import credentials, firestore, initialize_app
 
-UPLOAD_FOLDER = 'D:\\DACN2\\bao_ve_cay_trong\\back_end\\flask_app\\uploads'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-# def allowed_file(filename):
-#     extension = filename.rsplit('.', 1)[1].lower()
-#     return '.' in filename and extension in ALLOWED_EXTENSIONS
+cred = credentials.Certificate(
+    'plant-e7169-firebase-adminsdk-vv9mb-6fba92240c.json')
+default_app = initialize_app(cred)
+db = firestore.client()
+todo_ref = db.collection('Disease')
+
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 def model():
     global model
-    model = load_model('model.h5')
+    model = load_model('model_tool_train.h5')
 
+def pretrain(img):
+  image = cv2.cvtColor(np.float32(img), cv2.COLOR_BGR2RGB)
+  image = cv2.resize(image, (256,256))
+  image = np.reshape(image, (256,256, 3))
+  image = image /255.
+  return image
 
 @app.route('/', methods=['GET'])
 def home():
@@ -38,41 +46,24 @@ def home():
         "api": "[POST] /predict"
     }), 200
 
-
-# @app.route('/predict', methods=['POST'])
-# def predict():
-#     if 'img' not in request.files:
-#         return jsonify({
-#             'message': 'Missing file'
-#         }), 100
-#     file = request.files['img']
-#     if file.filename == '':
-#         return jsonify({
-#             'message': 'No file selected'
-#         }), 200
-
-#     if file and allowed_file(file.filename):
-#         try:
-#             filestr = file.read()
-#             img = np.frombuffer(filestr, np.uint8)
-#             img = cv2.imdecode(img, cv2.IMREAD_COLOR)
-#             img = cv2.resize(img, (128, 128, 3))
-#             # img = cv2.reshape(img, (128, 128, 3))
-#             # img = np.expand_dims(img, 0)
-#             pred = model.predict(img)
-#             pred = np.argmax(np.round(pred), axis=0)
-#             return jsonify({
-#                 'message': 'Success',
-#                 'traffic_id': str(pred[0])
-#             })
-#         except:
-#             return jsonify({
-#                 'message': 'Server error'
-#             }), 300
-#     else:
-#         return jsonify({
-#             'message': 'File doesn\'t support'
-#         }), 400
+@app.route('/list', methods=['GET'])
+def read():
+    """
+        read() : Fetches documents from Firestore collection as JSON.
+        todo : Return document that matches query ID.
+        all_todos : Return all documents.
+    """
+    try:
+        # Check if ID was passed to URL query
+        todo_id = request.args.get('id')
+        if todo_id:
+            todo = todo_ref.document(todo_id).get()
+            return jsonify(todo.to_dict()), 200
+        else:
+            all_todos = [doc.to_dict() for doc in todo_ref.stream()]
+            return jsonify(all_todos), 200
+    except Exception as e:
+        return f"An Error Occured: {e}"
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -90,28 +81,20 @@ def predict():
                 "error": True,
                 'message': 'No file selected'
             }), 200
-        if file and allowed_file(file.filename):
-            filename = Image.open(file)
-            filename = secure_filename(file.filename)
-            print(filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return "Upload Successfully" + filename, 200
-        # if file and allowed_file(file.filename):
         try:
-            # filestr = file.read() 
-            img = Image.open(file)
-
-            # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-            img = img/255.
-            img = cv2.reshape(img, (1, 128, 128, 3))
+            if file and allowed_file(file.filename):
+                filename = Image.open(file)   
+            img = pretrain(filename)
             img = image.img_to_array(img)
+            cv2.imshow("ded", img)
+            cv2.waitKey(0)
             img = np.expand_dims(img, 0)
             pred = model.predict(img)
             pred = np.argmax(np.round(pred), axis=0)
             return jsonify({
                 'filename': str(file.filename),
                 'message': 'Success',
-                'name': str(pred),
+                'name': str(pred[0]),
             })
         except Exception as e:
             print (e)
@@ -124,7 +107,7 @@ def predict():
         print(e)
         return f"An Error Occured: {e}"
 
-
+port = int(os.environ.get('PORT', 8080))
 if __name__ == "__main__":
 
     # port = int(os.environ.get('PORT', 8080))
@@ -132,4 +115,5 @@ if __name__ == "__main__":
     print(("* Loading Keras model and Flask starting server..."
            "please wait until server has fully started"))
     model()
-    app.run(threaded=True, host='0.0.0.0', port=8080)
+    # app.run(threaded=True, host='0.0.0.0', port=8080)
+    app.run(threaded=True, host='0.0.0.0', port=port)
--- back_end/testModel.py
+++ back_end/testModel.py
@@ -9,18 +9,15 @@ import matplotlib.pylab as plt
 
 data = r'validation'
 imagePaths = list(paths.list_images(data))
-classes =  0
-lb = LabelEncoder()
+# classes =  0
+# lb = LabelEncoder()
 
 labels = ['BrownSpot', 'Healthy', 'Hispa', 'LeafBlast', 'LeafBlight', 'SheathBlight', 'Tungro']
-lb.fit_transform(labels)
-classes = lb.classes_
 
-model = load_model('model_tool_train.h5')
+model = load_model('model.h5')
 
 def pretrain(img):
-  image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-  image = cv2.resize(image, (256,256))
+  image = cv2.resize(img, (256,256))
   image = np.reshape(image, (256,256, 3))
   image = image /255.
   return image
@@ -30,16 +27,19 @@ def predict(image):
     probabilities = model.predict(np.asarray([image]))[0]
     class_idx = np.argmax(probabilities)
     
-    return {labels[class_idx]: probabilities[class_idx]}
+    return {labels[class_idx]: probabilities[class_idx]*(99/101)}
 
-for idx, imagePath in enumerate(random.sample(imagePaths, 10)):
+# for idx, imagePath in enumerate(random.sample(imagePaths, 10)):
+for idx, imagePath in enumerate(imagePaths):
     print("SOURCE: name: %s, path: %s" % (os.path.split(imagePath)[-1], imagePath))
     
-    image = cv2.imread(imagePath)
-    image = pretrain(image)
+    img = cv2.imread(imagePath)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    image = pretrain(img)
     # image = np.expand_dims(image, 0)
     prediction = predict(image)
     print("PREDICTED: class: %s, confidence: %f" % (list(prediction.keys())[0], list(prediction.values())[0]))
-    plt.imshow(image)
+    plt.title("name: %s,\n class: %s,\n confidence: %f" % (os.path.split(imagePath)[-1], list(prediction.keys())[0], list(prediction.values())[0]))
+    plt.imshow(img)
     # plt.figure(idx)    
     plt.show()
